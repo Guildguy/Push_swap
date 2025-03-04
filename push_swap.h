19ebb7aa4994539eb58731edef6f6e90e59fede2
root <root@DESKTOP-12PJ67V>
2025-03-04 11:28:11 -0300

    adding some sorter functions

--- push_swap.h
+++ push_swap.h
@@ -55,17 +55,48 @@ int		print_ptr(void *pointer);
 ////printf
 int		format_specifier(va_list arg, const char format);
 int		ft_printf(char *str, ...);
-//p_split
+//moves
+////moves_utils
+void	set_top_a(t_node **stack, t_node *top);
+void	set_top_b(t_node **stack, t_node *top);
+void	rotate_a_b(t_node **stack_a, t_node **stack_b, t_node *shortest);
+void	reverse_a_b(t_node **stack_a, t_node **stack_b, t_node *shortest);
+////push
+void	pa(t_node **stack_a, t_node **stack_b);
+void	pb(t_node **stack_b, t_node **stack_a);
+////reverse
+void	rra(t_node **stack);
+void	rrb(t_node **stack);
+void	rrr(t_node **stack_a, t_node **stack_b);
+////rotate
+void	ra(t_node **stack);
+void	rb(t_node **stack);
+void	rr(t_node **stack_a, t_node **stack_b);
+////swap
+void	sa(t_node **stack);
+void	sb(t_node **stack);
+void	ss(t_node **stack_a, t_node **stack_b);
+//sorter
+////prep_a
+void	set_position(t_node *stack);
+void	prepare_a(t_node *stack_a, t_node *stack_b);
+////
+//main
+////ft_lstlast
+t_node	*ft_lstlast(t_node *lst);
+////ft_lstsize
+int		ft_lstsize(t_node *lst);
+////p_split
 void	free_matrix(char *av[]);
 char	**push_split(char *str, char separator);
-//push_utils
+////populate_stack
+void	populate_stack(t_node **stack, t_node **ptr, bool array);
+////push_utils
 void	deallocate_node(t_node **root);
 bool	stack_done(t_node *stack);
 t_node	*find_smallest(t_node *stack);
 t_node	*find_largest(t_node *stack);
 t_node	*get_shortest(t_node *stack);
-//ft_lstlast
-t_node	*ft_lstlast(t_node *lst);
 //populate stack
 void	populate_stack(t_node **stack, t_node **ptr, bool array);
 
