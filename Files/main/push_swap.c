31bd531cdd1519f43df63592a8f4d6ebdec9a266
Fabio Luiz Leite Junior <fabiolljunior23@gmail.com>
2025-03-31 15:56:23 -0300

    adding checker and changing 1 arg test

--- Files/main/push_swap.c
+++ Files/main/push_swap.c
@@ -12,18 +12,18 @@
 
 #include "../../push_swap.h"
 
-int	main(int ac, char *av[])
+int	main(int c, char *v[])
 {
 	t_node	*a;
 	t_node	*b;
 
 	a = NULL;
 	b = NULL;
-	if (ac == 1 || (ac == 2 && !av[1][0]))
+	if (c == 1 || (c == 2 && v[1][0]))
 		return (0);
-	else if (ac == 2)
-		av = push_split(av[1], ' ');
-	populate_stack(&a, av + 1, ac == 2);
+	else if (c == 2)
+		v = push_split(v[1], ' ');
+	populate_stack(&a, v + 1, c == 2);
 	if (!stack_done(a))
 	{
 		if (ft_lstsize(a) == 2)
